792370f32d0c8b3c47492fa8dece04da05405400
M.Hassoun <96958050+Hassoun00@users.noreply.github.com>
2022-09-16 19:34:05 +0300

    Initial Commit

---
+++ complexAPI/complexAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  complexAPIApp.swift
+//  complexAPI
+//
+//  Created by hassoun on 16/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct complexAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
